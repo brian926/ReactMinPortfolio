714071161752206b2340a2153e72eb4793fee9cc
Brian Antunes <brian.antunes@yahoo.com>
2022-11-22 09:53:32 -0500

    Added navbar tailwind

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,21 +6,19 @@ import Home from '../home/home'
 function NavBar() {
     return (
         <Router>
-        <div>
-            <nav>
-            <ul>
-                <li>
-                <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/About">About</Link>
-                </li>
-                <li>
-                <ThemeIcon />
-                </li>
-            </ul>
+            <nav className='flex items-center justify-between flex-wrap p-6'>
+                <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
+                    <div className='text-sm lg:flex-grow'>
+                        <Link className='block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4' to="/">Home</Link>
+                    </div>
+                    <div className='text-sm lg:flex-grow'>
+                        <Link className='block mt-4 lg:inline-block lg:mt-0 hover:text-white mr-4' to="/about">About</Link>
+                    </div>
+                    <div>
+                        <ThemeIcon className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border white hover:border-transparent hover:bg-white mt-4 lg:mt-0' />
+                    </div>
+                </div>
             </nav>
-        </div>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
