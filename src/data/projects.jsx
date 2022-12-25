94df5807f543d9051d0331c52e20148f383df221
Brian Antunes <brian.antunes@yahoo.com>
2022-12-25 11:29:40 -0500

    Added better descriptions

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,30 +1,30 @@
 export default [
     {
-        title: 'Star Gazer\'s NASA Apod',
+        title: 'Star Gazer\'s Apod',
         imgUrl: '/assets/apod.png',
         stack: ['NodeJS', 'Express', 'JavaScript', 'Bootstrap'],
         link: 'https://star-gazers-apod.onrender.com',
-        description: 'This site was built in hope to help share and spread the amazing information provided straight from NASA to better appreciate space. Here you can view daily astronomy photos along with signing up for our Daily Newsletter!',
+        description: "Welcome to Star Gazer\'s Apod, where we share and spread the fascinating information provided by NASA to help appreciate space. On Star Gazer\'s Apod, you can view daily astronomy photos and sign up for our Daily Newsletter.",
     },
     {
         title: 'The Quarry Barbershop',
         imgUrl: '/assets/thequarry.png',
         stack: ['HTML', 'CSS'],
         link: 'https://thequarrybarbershop.netlify.app',
-        description: 'A barbershop website that displays the team members of the Quarry Barbershop, hours of operation, and you can even book an appointment online.',
+        description: "Welcome to the Quarry Barbershop website! Here, you can meet our team, view our hours of operation, and even book an appointment online.",
     },
     {
         title: 'Azure Resume',
         imgUrl: '/assets/azureresume.png',
         stack: ['HTML', 'CSS', 'JavaScript', 'C#'],
         link: 'https://www.brianantunes.com/',
-        description: 'My own personal website that is hosted completely within Azure. The frontend is utilizing Azure storaget to host a static site which then backs calls to our backend Azure Database.',
+        description: "This is my personal website, hosted on Azure. The frontend uses Azure storage to host a static site, while the backend utilizes an Azure database. Feel free to explore and learn more about me and my work.",
     },
     {
         title: 'Brian\'s Portfolio',
         imgUrl: '/assets/portfolio.png',
         stack: ['Vite', 'React', 'Tailwind', 'JavaScript'],
         link: 'https://github.com/brian926/ReactMinPortfolio',
-        description: 'This website! Check out the GitHub Repo here!',
+        description: "Welcome to this website! You can check out the GitHub repository for it here.",
     },
 ]
